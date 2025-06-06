91fba459b1d3147545e2f261f6ffa4bbb2430f52
taufiqdp <topexdw@gmail.com>
2025-06-06 14:48:07 +0700

    Remove reload dev

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,8 @@
-import os
-
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes.agent import router as agent_router
 from src.routes.transaction import router as transaction_router
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = FastAPI()
 
@@ -33,4 +26,4 @@ app.include_router(router=transaction_router, prefix="/api/v1", tags=["transacti
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
